dbac7014247abdeede71f4ce4b392989d0cc50e5
vladimir <vladimir@olmo-Vostro-460>
2018-04-23 16:33:39 -0500

    current position and background layers was added

--- src/java/com/mapviewer/model/Layer.java
+++ src/java/com/mapviewer/model/Layer.java
@@ -86,7 +86,11 @@ public class Layer {
 
 	// ---- To bypass routers which block local address for DNS Rebinding Attacks
 	private String localAddress;// Indicate the local addresss of the server (to request layer details)
-	
+
+    // ---- zoom and position options
+    private Byte zoom;//zoom in map
+    private Point center;//longitude
+
 	/**
 	 * Verify that the input MenuEntry correspond to this layer
 	 *
@@ -164,6 +168,10 @@ public class Layer {
 		this.overlayStreamlines = "";
 		this.defParticleSpeed = 1.0f;//By default we don't change it
                 this.isParticle="false";//Stores if it is a particle or not
+        
+        //default zoom and center position is null
+        this.zoom = null;
+        this.center = null;
 	}
 	/**
 	 *
@@ -257,9 +265,12 @@ public class Layer {
 		this.overlayStreamlines = overlayStreamlines;
 		this.defParticleSpeed = defParticleSpeed;
 		this.localAddress = null;
-                this.belowMinColor = belowMinColor;
-                this.aboveMaxColor = aboveMaxColor;
                 this.isParticle = isParticle;
+        this.belowMinColor = belowMinColor;
+        this.aboveMaxColor = aboveMaxColor;
+        //default zoom and center position is null
+        this.zoom = null;
+        this.center = null;
 	}
 	//Geters
 	
@@ -275,7 +286,6 @@ public class Layer {
 	 * @param {String} layerDetails
 	 */
 	public void setLayerDetails(String layerDetailsStr) {
-		
 		// In this case we have to update the min and max color with the
 		// default color range of the layer.
 		// We add the 'server' and the 'name' into the layer Details
@@ -297,7 +307,7 @@ public class Layer {
 				this.zaxis = true;
 			}
 			
-			//If the style is not defined in the XML file. Then we use the first one
+			//If the style is not defined in the XML file. Then we use the first
 			// inside the layerDetails option 'supportedStyles' from ncWMS
 			// This should be: boxfill for ncwms 1 and default-scalar for ncWMS 2
 			if(layerDetails.has("supportedStyles") && this.style.equals("")){
@@ -355,8 +365,16 @@ public class Layer {
             if( !this.featureInfoLayer.equals(name) ){
                 layerDetails.accumulate("featureInfo", this.featureInfoLayer);
             }
-		} catch (JSONException ex) {
-			System.out.println("ERROR: The layerdetails JSON object can't be created on Layer class");
+
+            //Add zoom and center
+            String strZoom = String.valueOf(this.zoom);
+            String strCenter = this.center.toString();
+            layerDetails.accumulate("zoom", strZoom);
+            layerDetails.accumulate("center", strCenter);
+            
+        } catch (JSONException ex) {
+
+            System.out.println("ERROR: The layerdetails JSON object can't be created on Layer class");
 		}
 	}
 	
@@ -455,6 +473,23 @@ public class Layer {
 		return vectorLayer;
 	}
 	
+    
+    public Byte getZoom() {
+        return zoom;
+    }
+
+    public void setZoom(Byte zoom) {
+        this.zoom = zoom;
+    }
+
+    public Point getCenter() {
+        return this.center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
 	public void setLayout(String layout) {
 		this.layout = layout;
 	}
